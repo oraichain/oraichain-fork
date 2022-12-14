0bd6761b76c2408ba87be96e4cbc03e1786431f9
ducphamle2 <ducphamle212@gmail.com>
2022-12-13 19:28:03 -0800

    remove auto download in index.js & move to export geneis script instead

--- export-genesis.sh
+++ export-genesis.sh
@@ -2,6 +2,11 @@
 
 NEED_TO_RUN=${NEED_TO_RUN:-1}
 
+if [[ ! -f "./genesis.json" ]]
+then
+    wget -O ./genesis.json https://orai.s3.us-east-2.amazonaws.com/export-genesis.json
+fi
+
 if [[ -f "./export-genesis.json" ]]
 then
     echo 'Already has export-genesis.json exported. Nothing to do!'
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cp = require('child_process');
 const exportGenesisJsonPath = './export-genesis.json';
 const wantedGenesisStateJsonPath = './genesis.json';
 const genesisJsonPath = '.oraid/config/genesis.json';
-const genesisJsonUrl = 'https://orai.s3.us-east-2.amazonaws.com/export-genesis.json';
 
 const readGenesis = () => {
     if (!fs.existsSync(exportGenesisJsonPath)) {
@@ -18,11 +17,6 @@ const readGenesis = () => {
     const validators = JSON.stringify(result.validators);
 
     const jq = `'.app_state.slashing = ${slashing} | .app_state.staking = ${staking} | .app_state.distribution = ${distribution} | .validators = ${validators} | .app_state.staking.params.unbonding_time = "10s" | .app_state.gov.voting_params.voting_period = "60s" | .app_state.gov.deposit_params.min_deposit[0].amount = "1" | .app_state.gov.tally_params.quorum = "0.000000000000000000" | .app_state.gov.tally_params.threshold = "0.000000000000000000" | .app_state.mint.params.inflation_min = "0.500000000000000000" | .chain_id = "Oraichain-fork"'`
-
-    // if genesis.json does not exist, then we download it
-    if (!fs.existsSync(wantedGenesisStateJsonPath)) {
-        cp.execSync(`wget -O ${wantedGenesisStateJsonPath} ${genesisJsonUrl}`);
-    }
 
     cp.exec(`jq ${jq} ${wantedGenesisStateJsonPath} > ${genesisJsonPath}`, (err, stdout, stderr) => {
         if (err) {
