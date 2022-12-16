9d9ca457f9f36b347f128c91eceb567e360405d8
ducphamle2 <ducphamle212@gmail.com>
2022-12-16 10:43:49 -0800

    remove test

--- test.js
+++
@@ -1,3 +0,0 @@
-console.log(Buffer.from(`"${Buffer.from('ACE2B31E144AC57F99807BA85D299A7B1A755E2F', 'hex').toString('base64')}"`).toString('base64'))
-console.log(Buffer.from("InJPS3pIaFJLeFgrWmdIdW9YU21hZXhwMVhpOD0i", 'base64').toString())
-console.log(Buffer.from("rOKzHhRKxX+ZgHuoXSmaexp1Xi8=", 'base64').toString('hex'))
